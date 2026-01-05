549c565403844d038a28a72bbf75fd76f784ebee
MikeSoroka <mihol2006@gmail.com>
2026-01-05 21:41:19 +0200

    Tensor class base created

---
+++ src/RitterFramework.Core/Tensor.cs
@@ -0,0 +1,154 @@
+namespace RitterFramework.Core;
+
+public class Tensor
+{
+    private float[] _data;
+    private int[] _shape;
+    private int[] _strides;
+    
+    public Tensor? Gradient { get; set; }
+    public bool RequiresGrad { get; set; }
+    public List<Tensor>? Parents { get; set; }
+    public Action<Tensor>? BackwardFn { get; set; }
+    
+    public int[] Shape => _shape;
+    public int Size => _data.Length;
+    public int Dimensions => _shape.Length;
+
+    public Tensor(float[] data, int[] shape, bool requiresGrad = false)
+    {
+        _data = data;
+        _shape = shape;
+        _strides = ComputeStrides(shape);
+
+        RequiresGrad = requiresGrad;
+
+        if (requiresGrad)
+        {
+            Gradient = Zeros(shape);
+        }
+    }
+
+    public static Tensor Zeros(int[] shape)
+    {
+        if(shape.Length < 1) throw new ArgumentOutOfRangeException(nameof(shape));
+        
+        var length =  1;
+        foreach(var dimension in shape) length *= dimension;
+        
+        return new Tensor(new float[length], shape);
+    }
+    
+    public static Tensor Ones(int[] shape)
+    {
+        if(shape.Length < 1) throw new ArgumentOutOfRangeException(nameof(shape));
+        
+        var length =  1;
+        foreach(var dimension in shape) length *= dimension;
+        
+        var contents = new float[length];
+        for (int i = 0; i < length; i++)
+        {
+            contents[i] = 1;
+        }
+        
+        return new Tensor(contents, shape);
+    }
+    
+    public float this[int[] indices]
+    {
+        get =>
+            _data[GetFlatIndex(indices)];
+        set =>
+            _data[GetFlatIndex(indices)] = value;
+    }
+
+    public static Tensor operator +(Tensor tensorA, Tensor tensorB)
+    {
+        if (!tensorA._shape.SequenceEqual(tensorB._shape))
+            throw new ArgumentException("Shapes must match for addition");
+        
+        var newContents = tensorA._data.Zip(tensorB._data, (a, b) => a + b).ToArray();
+        var newTensor = new Tensor(newContents, tensorA._shape, tensorA.RequiresGrad || tensorB.RequiresGrad);
+            
+        if (tensorA.RequiresGrad || tensorB.RequiresGrad)
+        {  
+            newTensor.Parents = new List<Tensor>{tensorA, tensorB};
+            newTensor.BackwardFn = gradOutput =>
+            {
+                if (tensorA.RequiresGrad)
+                {
+                    tensorA._data = tensorA._data.Zip(gradOutput._data, (a, g) => a + g).ToArray();
+                }
+
+                if (tensorB.RequiresGrad)
+                {
+                    tensorB._data = tensorB._data.Zip(gradOutput._data, (b, g) => b + g).ToArray();
+                }
+            };
+        }
+            
+        return newTensor;
+    }
+    
+    public static Tensor operator *(Tensor tensor, float scalar)
+    {
+        var newContents = tensor._data.Select(a => a * scalar).ToArray();
+        var newTensor = new Tensor(newContents, tensor._shape, tensor.RequiresGrad);
+            
+        if (tensor.RequiresGrad)
+        {  
+            newTensor.Parents = new List<Tensor>{tensor};
+            newTensor.BackwardFn = gradOutput =>
+            {
+                tensor._data = tensor._data.Select(a => a * scalar).ToArray();
+            };
+        }
+            
+        return newTensor;
+    }
+
+    public void Backward(Tensor? gradOutput = null)
+    {
+        if (gradOutput == null)
+        {
+            if (Size != 1)
+                throw new ArgumentException("Gradient must be provided for non-scalar tensors");
+            
+            gradOutput = Ones(Shape);
+        }
+
+        if(Gradient == null) Gradient = Zeros(Shape);
+        
+        // TODO: Increment global pass ID
+        // TODO: Implement further Backward trigger
+        for (int i = 0; i < Size; i++)
+            Gradient._data[i] += gradOutput._data[i];
+        
+        BackwardFn?.Invoke(gradOutput);
+    }
+    
+    private int[] ComputeStrides(int[] shape)
+    {
+        var strides = new int[shape.Length];
+        var stride = 1;
+
+        for (var i = shape.Length - 1; i >= 0; i--)
+        {
+            strides[i] = stride;
+            stride *= shape[i];
+        }
+        
+        return strides;
+    }
+
+    private int GetFlatIndex(int[] indices) =>
+        indices.Zip(_strides, (idx, stride) => new { idx, stride })
+            .Select((item, i) => 
+            {
+                if (item.idx < 0 || item.idx >= _shape[i])
+                    throw new IndexOutOfRangeException($"Index {item.idx} at dimension {i} is out of bounds.");
+               
+                return item.idx * item.stride;
+            }).Sum();
+}
